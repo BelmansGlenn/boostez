5450debdec50e2183a906989f5bed5549fce8f9a
BelmansGlenn <belmansglenn@gmail.com>
2022-04-14 08:43:21 +0200

    bibliography test

---
+++ src/Controller/BibliographyController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BibliographyController extends AbstractController
+{
+    private $locales;
+    private $defaultLocale;
+
+    public function __construct($locales, $defaultLocale)
+    {
+        $this->locales = $locales;
+        $this->defaultLocale = $defaultLocale;
+    }
+
+    #[Route(['FR' => '/bibliographie', 'NL' => '/bibliografie', 'EN' => '/bibliography'], name: 'app_bibliography')]
+    public function getBibliography(): Response
+    {
+
+        return $this->render('bibliography/index.html.twig');
+    }
+}
