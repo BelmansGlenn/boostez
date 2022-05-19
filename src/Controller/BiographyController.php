00ebeae67afe276efff5db7e47c2de6ed579967e
dotcom <dotcom@MacBook-Air-de-dotcom.local>
2022-05-19 15:57:46 +0200

    carou

--- src/Controller/BiographyController.php
+++ src/Controller/BiographyController.php
@@ -6,17 +6,19 @@ use App\Repository\LogoRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\NewsletterFormType;
 
 class BiographyController extends AbstractController
 {
     #[Route(['FR' => '/biographie', 'NL' => '/biografie', 'EN' => '/biography'], name: 'app_biography')]
     public function index(LogoRepository $logoRepository): Response
     {
-
+        $form = $this->createForm(NewsletterFormType::class);
        $logo = $logoRepository->findAllLogoByOrderByImportance();
 
         return $this->render('biography/index.html.twig', [
-            'logo' => $logo
+            'logo' => $logo,
+            'form' => $form->createView()
         ]);
     }
 }
